b9493ab51ff74df80481ff251ff6f64d9e790dae
memsb <mbuckley@gmail.com>
2018-04-18 20:28:09 +0100

    Changing deployment to upload files without html extension

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Purge bucket
 time aws s3 rm s3://martin-laura.com --recursive
+
+# List html files
+declare -a arr=(public/*.html)
+
+# rename to remove .html
+for filename in "${arr[@]}"; do mv "$filename" "${filename%.html}"; done
+
+# Upload pages
 time aws s3 sync --exclude="assets/*" public s3://martin-laura.com --acl public-read --content-type "text/html"
+
+# Upload assets
 time aws s3 sync --exclude="*" --include="assets/*" public s3://martin-laura.com --acl public-read
+
+# Rename to add .html
+for filename in "${arr[@]}"; do mv  "${filename%.html}" "$filename"; done
